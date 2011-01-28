8087feb914d376445ba36c98d3bd4171134ef8b1
郭靖 <soundbbg@gmail.com>
2011-01-28 14:44:32 +0800

    make ga_sender easy to use, add example.

--- ga_sender/ga_sender.py
+++ ga_sender/ga_sender.py
@@ -1,6 +1,10 @@
-er
+#!/usr/bin/env python
 # -*- coding: UTF-8 -*-r
 
+import sys
+import uuid
+import socket
+import httplib2
 import time
 from urllib import quote 
 from random import randint
@@ -56,12 +60,14 @@ class GoogleAnlayticsMobile(object):
         self.__utmb = str(self.ga_hash) + '.1.10.' + self.time
         self.__utmc = str(self.ga_hash)
         self.__utmz = str(self.ga_hash) + '.' + self.time + '.1.1.' + source_str
+        utmz = quote(self.__utmz)
         self.img_url = str(self.ga_img) + '?utmwv=' + self.ga_utmwv + \
             '&utmn=' + str(randint(1000000000, 9999999999)) + '&utmhn=' + \
             self.ga_utmhn + '&utmhid=' + str(randint(1000000000, 9999999999)) + \
-            '&utmr=' + (quote(self.ga_referer) or '-') + '&utmp=' + quote(uri) + \
+            '&utmr=' + quote(env.get('referer') or '-') + \
+            '&utmp=' + quote(env.get('url')) + \
             '&utmac=' + self.ga_utmac + '&utmcc=__utma%3D' + self.__utma + \
-            '%3B%2B__utmz%3D' + quote(self.__utmz) + '%3B'
+            '%3B%2B__utmz%3D' + utmz + '%3B'
 
     def gen_hash(self, ga_utmhn):
         if not ga_utmhn:
@@ -87,9 +93,31 @@ class GoogleAnlayticsMobile(object):
             return dict(utmgclid='', utmcsr=env.get('utm_source',''), utmccn=utmccn, utmctr=env.get('utm_term',''), utmcct=utmcct)
         elif self.ga_referer:
             return dict(utmgclid='', utmcsr='(direct)', utmccn='(direct)', utmcmd='(none)', utmctr='', utmcct='')
+        return dict(utmgclid='', utmcsr='(direct)', utmccn='(direct)', utmcmd='(none)', utmctr='', utmcct='')
 
     def get_imgurl(self):
         return self.img_url
 
-#g = GoogleAnlayticsMobile('UA-XXXXX-XX', 'www.domain.com', environ)
-#g.get_imgurl()
+def send_request_to_google_analytics(utm_url, environ):
+    http = httplib2.Http()    
+    try:
+        resp, content = http.request(utm_url, 
+            'GET', 
+            headers={'User-Agent': environ.get('user_agent', 'Unknown')}
+        )
+        socket.setdefaulttimeout(10)
+        if resp.get('status') != '200':
+            print resp
+        else:
+            print '%s Sended' % environ.get('ip')
+            #print utm_url
+            pass
+    except:
+        pass
+
+def send_to_ga(environ):
+    g = GoogleAnlayticsMobile('UA-15296817-5', 'www.jguoer.com', environ)
+    send_request_to_google_analytics(g.get_imgurl(), environ)
+
+e = {'domain': 'www.jguoer.com', 'protocol': 'HTTP/1.1', 'uid': '39454209', 'status_code': '200', 'url': '/?session=3fd90546_39454209', 'ip': '122.192.166.136', 'bid': '4979797d', 'time': '2011-1-28 14:40:35 +0800', 'page_size': '3821', 'referer': 'http://www.jguoer.com/?session=6acf607a_39454209', 'user_agent': 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_1 like Mac OS X; zh-cn) AppleWebKit/532.9 (KHTML, like Gecko) Version/4.0.5 Mobile/8B117 Safari/6531.22.7', 'process_time': '0', 'method': 'GET'}
+send_to_ga(e)
